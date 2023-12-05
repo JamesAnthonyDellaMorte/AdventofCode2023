20271cae07c4d35efc905ab890dc09b25df62288
James A DellaMorte <james.a@popotomodem.com>
2023-12-05 01:55:13 -0500

    day3 part 2 wrong, but close

--- day3/three.cpp
+++ day3/three.cpp
@@ -1,4 +1,5 @@
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <numeric>
@@ -136,13 +137,124 @@ int main(int argc, char *argv[]) {
       }
   }
 
-  for (int num : numbers) {
-    std::cout << num << " ";
-  }
-  std::cout << std::endl;
-
+  std::vector<std::vector<std::string>> gearMatrix;
+  std::vector<int> gearRatio;
   std::cout << "Sum: " << std::accumulate(numbers.begin(), numbers.end(), 0)
             << std::endl;
 
+  // loop through matrix and replace mulitple digits number with a single string
+  // number
+  for (size_t i = 0; i < matrix.size(); i++) {
+    gearMatrix.push_back(std::vector<std::string>());
+    for (size_t j = 0; j < matrix[i].size(); j++)
+
+      if (is_number(matrix[i][j])) {
+        std::string number = matrix[i][j];
+        int k = j + 1;
+        while (k < matrix[i].size() && matrix[i][k] != "." &&
+               is_symbol(matrix[i][k]) == false) {
+          number += matrix[i][k];
+          k++;
+        }
+        // add the number equal to number of digits to the matrix
+
+        gearMatrix[i].push_back(number);
+
+        int digits = k - j - 1;
+        j += digits;
+        for (int q = 0; q < digits; q++) {
+          gearMatrix[i].push_back(number);
+        }
+
+      } else {
+        gearMatrix[i].push_back(matrix[i][j]);
+      }
+  }
+
+  for (size_t i = 0; i < gearMatrix.size(); i++) {
+    for (size_t j = 0; j < gearMatrix[i].size(); j++) {
+      std::cout << gearMatrix[i][j];
+    }
+    std::cout << std::endl;
+  }
+  for (size_t i = 0; i < gearMatrix.size(); i++) {
+    for (size_t j = 0; j < gearMatrix[i].size(); j++) {
+      if (is_symbol(matrix[i][j])) {
+        // check whether the symbol is touching a two numbers if it is muliply
+        // them and add to gearRatio above
+        std::vector<int> temp_array;
+
+        // above
+        if (i > 0) {
+          if (is_number(gearMatrix[i - 1][j])) {
+            temp_array.push_back(std::stoi(gearMatrix[i - 1][j]));
+          }
+        }
+        // below
+        if (i < gearMatrix.size() - 1) {
+          if (is_number(gearMatrix[i + 1][j])) {
+            temp_array.push_back(std::stoi(gearMatrix[i + 1][j]));
+          }
+        }
+        // right
+        if (j < gearMatrix.size()) {
+          if (is_number(gearMatrix[i][j + 1])) {
+            temp_array.push_back(std::stoi(gearMatrix[i][j + 1]));
+          }
+        }
+        // left
+        if (j != 0) {
+          if (is_number(gearMatrix[i][j - 1])) {
+            temp_array.push_back(std::stoi(gearMatrix[i][j - 1]));
+          }
+        }
+        // diagonal left up
+        if (i > 0 && j > 0 && j < gearMatrix.size()) {
+          if (is_number(gearMatrix[i - 1][j - 1])) {
+            temp_array.push_back(std::stoi(gearMatrix[i - 1][j - 1]));
+          }
+        }
+        // diagonal right up
+        if (i > 0 && j < gearMatrix[i].size()) {
+          if (is_number(gearMatrix[i - 1][j + 1])) {
+            temp_array.push_back(std::stoi(gearMatrix[i - 1][j + 1]));
+          }
+        }
+        // diagonal left down
+        if (i < gearMatrix.size() - 1 && j > 0) {
+          if (is_number(gearMatrix[i + 1][j - 1])) {
+            temp_array.push_back(std::stoi(gearMatrix[i + 1][j - 1]));
+          }
+        }
+        // diagonal right down
+        if (i < gearMatrix.size() - 1 && j < gearMatrix.size()) {
+          if (is_number(gearMatrix[i + 1][j + 1])) {
+            temp_array.push_back(std::stoi(gearMatrix[i + 1][j + 1]));
+          }
+        }
+        // check if ever val is equal in temp_array
+        if (std::equal(temp_array.begin() + 1, temp_array.end(),
+                       temp_array.begin())) {
+          if (temp_array.size() > 2) {
+            gearRatio.push_back(temp_array[0] * temp_array[1]);
+            break;
+          }
+        }
+
+        std::sort(temp_array.begin(), temp_array.end());
+        temp_array.erase(std::unique(temp_array.begin(), temp_array.end()),
+                         temp_array.end());
+        if (temp_array.size() >= 2) {
+          // remove duplicates
+
+          gearRatio.push_back(temp_array[0] * temp_array[1]);
+          break;
+        }
+      }
+    }
+  }
+  std::cout << "Gear Ratio: "
+            << std::accumulate(gearRatio.begin(), gearRatio.end(), 0)
+            << std::endl;
   return 0;
 }
